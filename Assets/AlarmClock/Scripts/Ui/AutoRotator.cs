db7a110ea81c886829e5b16baad4d900acdd74d2
Rodion <workavast@yandex.ru>
2024-09-05 14:31:59 +0300

    create loading marker

---
+++ Assets/AlarmClock/Scripts/Ui/AutoRotator.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace AlarmClock.Scripts.Ui
+{
+    public class AutoRotator : MonoBehaviour
+    {
+        [SerializeField] private float rotationSpeed = 1;
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.back * (rotationSpeed * Time.deltaTime));
+        }
+    }
+}
